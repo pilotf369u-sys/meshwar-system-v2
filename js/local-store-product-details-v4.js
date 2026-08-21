3c40fe1dbf0a96f7a8451adaaa29ddedbffdb8d3
pilotf369u-sys <pilotf369u@gmail.com>
2026-08-21 19:06:32 +0300

    chore: bust local store V4-V7 asset cache

--- js/local-store-product-details-v4.js
+++ js/local-store-product-details-v4.js
@@ -46,19 +46,19 @@
   },true);
 
   const script=document.createElement('script');
-  script.src='js/local-store-product-details-v4-core.js?v=ui-stock-v7-1';
+  script.src='js/local-store-product-details-v4-core.js?v=desc-unlimit-v8-1';
   script.dataset.mwProductDetailsV4Core='1';
   script.onload=()=>{
     const variant=document.createElement('script');
-    variant.src='js/local-store-variant-stock-v5.js?v=ui-stock-v7-1';
+    variant.src='js/local-store-variant-stock-v5.js?v=desc-unlimit-v8-1';
     variant.dataset.mwVariantStockV5='1';
     variant.onload=()=>{
       const matrix=document.createElement('script');
-      matrix.src='js/local-store-matrix-stock-v6.js?v=ui-stock-v7-1';
+      matrix.src='js/local-store-matrix-stock-v6.js?v=desc-unlimit-v8-1';
       matrix.dataset.mwMatrixStockV6='1';
       matrix.onload=()=>{
         const v7=document.createElement('script');
-        v7.src='js/local-store-ui-stock-v7.js?v=ui-stock-v7-1';
+        v7.src='js/local-store-ui-stock-v7.js?v=desc-unlimit-v8-1';
         v7.dataset.mwUiStockV7='1';
         v7.onload=()=>resolveCore();
         v7.onerror=()=>rejectCore(new Error('تعذر تحميل تحسينات V7.'));
